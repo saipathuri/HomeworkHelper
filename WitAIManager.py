e5db8decf9b3001c190debc51f13d5f488d98b8d
Abhishaike <abhishaike@gmail.com>
2017-03-04 20:19:28 -0600

    Update WitAIManager.py

--- WitAIManager.py
+++ WitAIManager.py
@@ -11,22 +11,22 @@ def send(request, response):
     print(response['text'])
 
 def merge(request): #meant to keep conversational context within wit.ai
-	try:
-		context = request['context'] #retrieve previously stored information
-		entities = request['entities'] #retrieve new information
-		key = entities.keys()[0] #get the list of all the keys stored in the first index of the dictionary
-		list_type_to_merge = entities[key] #get the list of all the items stored in the specified key
-		entity_name = "" #lmao wtf
-		for i in range(0,len(list_type_to_merge)): #go from 0 to the number of items stored in the specified key
-			type_to_merge = list_type_to_merge[i] #save a specific section of the list to a single variable
-			if i < len(list_type_to_merge)-1: #if the list_type_to_merge isn't being read at the last variable, add a space to the end of the word
-				entity_name += type_to_merge['value'] + " "
-			else:
-				entity_name += type_to_merge['value'] #if it is being read at the end of the last variable, don't add a space
-		context[key] = entity_name
-		return context #return all the gucci INFO
-	except IndexError:
-		print "Sorry, we didn't catch that. Can you try again?"
+	context = request['context'] #retrieve previously stored information
+	entities = request['entities'] #retrieve new information
+	key = entities.keys()[0] #get the list of all the keys stored in the first index of the dictionary
+	list_type_to_merge = entities[key] #get the list of all the items stored in the specified key
+	entity_name = "" #lmao wtf
+	for i in range(0,len(list_type_to_merge)): #go from 0 to the number of items stored in the specified key
+		type_to_merge = list_type_to_merge[i] #save a specific section of the list to a single variable
+		if i < len(list_type_to_merge)-1: #if the list_type_to_merge isn't being read at the last variable, add a space to the end of the word
+			entity_name += type_to_merge['value'] + " "
+		else:
+			entity_name += type_to_merge['value'] #if it is being read at the end of the last variable, don't add a space
+	context[key] = entity_name
+	return context #return all the gucci INFO
+
+
+
 
 def add_class(request): 
 	'''
@@ -56,7 +56,6 @@ actions = {
     'send': send,
     'merge': merge,
     'add_class': add_class,
-    'error': error,
 }
 
 client = Wit(access_token=access_token, actions=actions)
